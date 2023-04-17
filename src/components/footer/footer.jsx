56fe4fa756d48d25d9ab845265b441aea254f76d
loi02112001 <loi65732102@gmail.com>
2023-04-18 01:49:31 +0700

    fix ui and add slider homepage

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -6,44 +6,44 @@ export default function Footer() {
             <div className="footer__content">
                 <div className="footer__col">
                     <div className="footer__col-title">Mua xe</div>
-                    <ul class="footer__col-list">
-                        <li class="footer__col-list-item">Xe đã qua sử dụng</li>
-                        <li class="footer__col-list-item">Xe mới</li>
-                        <li class="footer__col-list-item">Chọn cấu hình xe</li>
-                        <li class="footer__col-list-item">Bảng giá</li>
+                    <ul className="footer__col-list">
+                        <li className="footer__col-list-item">Xe đã qua sử dụng</li>
+                        <li className="footer__col-list-item">Xe mới</li>
+                        <li className="footer__col-list-item">Chọn cấu hình xe</li>
+                        <li className="footer__col-list-item">Bảng giá</li>
                     </ul>
                 </div>
                 <div className="footer__col">
                     <div className="footer__col-title">Tư vấn mua xe</div>
-                    <ul class="footer__col-list">
-                        <li class="footer__col-list-item">Yêu cầu tư vấn</li>
-                        <li class="footer__col-list-item">Đăng ký lái thử</li>
-                        <li class="footer__col-list-item">Tìm nhà phân phối gần nhất</li>
+                    <ul className="footer__col-list">
+                        <li className="footer__col-list-item">Yêu cầu tư vấn</li>
+                        <li className="footer__col-list-item">Đăng ký lái thử</li>
+                        <li className="footer__col-list-item">Tìm nhà phân phối gần nhất</li>
                     </ul>
                 </div>
                 <div className="footer__col">
                     <div className="footer__col-title">Dịch vụ</div>
-                    <ul class="footer__col-list">
-                        <li class="footer__col-list-item">Đặt hẹn dịch vụ</li>
-                        <li class="footer__col-list-item">Ưu đãi đặc biệt</li>
-                        <li class="footer__col-list-item">Phụ kiện chính hãng</li>
+                    <ul className="footer__col-list">
+                        <li className="footer__col-list-item">Đặt hẹn dịch vụ</li>
+                        <li className="footer__col-list-item">Ưu đãi đặc biệt</li>
+                        <li className="footer__col-list-item">Phụ kiện chính hãng</li>
                     </ul>
                 </div>
                 <div className="footer__col">
                     <div className="footer__col-title">Các dòng xe</div>
-                    <ul class="footer__col-list">
-                        <li class="footer__col-list-item">SUV</li>
-                        <li class="footer__col-list-item">Sedan</li>
-                        <li class="footer__col-list-item">Mercedes-AMG</li>
-                        <li class="footer__col-list-item">Mercedes-Maybach</li>
+                    <ul className="footer__col-list">
+                        <li className="footer__col-list-item">SUV</li>
+                        <li className="footer__col-list-item">Sedan</li>
+                        <li className="footer__col-list-item">Mercedes-AMG</li>
+                        <li className="footer__col-list-item">Mercedes-Maybach</li>
                     </ul>
                 </div>
                 <div className="footer__col">
                     <div className="footer__col-title">Giới thiệu</div>
-                    <ul class="footer__col-list">
-                        <li class="footer__col-list-item">Liên hệ </li>
-                        <li class="footer__col-list-item">Câu chuyện của chúng tôi</li>
-                        <li class="footer__col-list-item">Tin tức và sự kiện</li>
+                    <ul className="footer__col-list">
+                        <li className="footer__col-list-item">Liên hệ </li>
+                        <li className="footer__col-list-item">Câu chuyện của chúng tôi</li>
+                        <li className="footer__col-list-item">Tin tức và sự kiện</li>
                     </ul>
                 </div>
             </div>
--- src/pages/homePage/homePage.jsx
+++ src/pages/homePage/homePage.jsx
@@ -1,5 +1,18 @@
-export default function HomePage(){
-    return(
-        <div>Đây là HomePage</div>
-    )
+import { Button, Carousel} from 'antd';
+import './homePage.scss';
+export default function HomePage() {
+    return (
+        <div className="homepage">
+            <div className="homepage__slider">
+                <div className="slider__content">
+                    <div className="slider__heading">Quà tặng đặc biệt</div>
+                    <div className="slider__button">
+                        <Button size='large' >Liên hệ tư vấn ngay</Button>
+                        <Button type='primary' size='large'>Chọn xe có sẵn cùng ưu đãi</Button>
+                    </div>
+                </div>
+            </div>
+            <div className="homepage__content"></div>
+        </div>
+    );
 }
