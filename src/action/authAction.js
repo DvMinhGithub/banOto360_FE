6b7185793b25ce4ecca2d3729ee42a387b2d7668
DvMinhGithub <dvminh.it@gmail.com>
2023-04-18 20:47:09 +0700

    Login redux

--- src/action/authAction.js
+++ src/action/authAction.js
@@ -5,6 +5,11 @@ const authActions = {
     loginRequest: createAction(authTypes.LOGIN_REQUEST),
     loginSuccess: createAction(authTypes.LOGIN_SUCCESS),
     loginFailure: createAction(authTypes.LOGIN_FAILURE),
+
+    registerRequest: createAction(authTypes.REGISTER_REQUEST),
+    registerSuccess: createAction(authTypes.REGISTER_SUCCESS),
+    registerFailure: createAction(authTypes.REGISTER_FAILURE),
+
     logoutRequest: createAction(authTypes.LOGOUT_REQUEST),
     logoutSuccess: createAction(authTypes.LOGOUT_SUCCESS),
     logoutFailure: createAction(authTypes.LOGOUT_FAILURE),
