4aa2a76d3ff6746b716c1ad1fd38187f752043ff
DvMinhGithub <dvminh.it@gmail.com>
2023-04-19 11:44:30 +0700

    Fix protect

--- src/layout/protectedComponent/protectedComponent.jsx
+++ src/layout/protectedComponent/protectedComponent.jsx
@@ -1,27 +1,24 @@
-import { useEffect } from "react"
-import { useLocation, useNavigate } from "react-router-dom"
+import { useEffect } from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
 
-
-export default function ProtectedComponent({children}){
-   const token =  localStorage.getItem('token')
-   const location = useLocation()
-   const navigate = useNavigate()
-   const {pathname} = location
-    useEffect(()=>{
-        if(token){
-            if(pathname === '/login' || pathname === '/register'){
-                navigate("/")
+export default function ProtectedComponent({ children }) {
+    const token = localStorage.getItem('token');
+    const location = useLocation();
+    const navigate = useNavigate();
+    const { pathname } = location;
+    useEffect(() => {
+        if (token) {
+            if (pathname === '/login' || pathname === '/register') {
+                navigate('/');
             }
-        }else{
-            if(pathname === '/login'){
-                navigate('/login')
-            }else if(pathname ==='register'){
-                navigate('register')
+        } else {
+            if (pathname === '/register') {
+                navigate('/register');
+            } else {
+                navigate('/login');
             }
         }
-    },  // eslint-disable-next-line
-    [])
-  
-
-    return children
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+    return children;
 }
