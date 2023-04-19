99f4fd97483b4625575c2c0bade4ca41e2d7b5da
DvMinhGithub <dvminh.it@gmail.com>
2023-04-19 18:10:51 +0700

    Remove car collection data

--- src/index.js
+++ src/index.js
@@ -13,19 +13,19 @@ import { persistor, store } from './store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    // <React.StrictMode>
-    <Provider store={store}>
-        <PersistGate persistor={persistor}>
-            <Router>
-                <ProtectedComponent>
-                    <GlobalStyle>
-                        <App />
-                    </GlobalStyle>
-                </ProtectedComponent>
-            </Router>
-        </PersistGate>
-    </Provider>
-    // </React.StrictMode>
+    <React.StrictMode>
+        <Provider store={store}>
+            <PersistGate persistor={persistor}>
+                <Router>
+                    <ProtectedComponent>
+                        <GlobalStyle>
+                            <App />
+                        </GlobalStyle>
+                    </ProtectedComponent>
+                </Router>
+            </PersistGate>
+        </Provider>
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
