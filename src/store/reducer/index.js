d088758b9ae9289af002a3908239abad928303db
DvMinhGithub <dvminh.it@gmail.com>
2023-04-19 22:27:24 +0700

    Update redux persist

--- src/store/reducer/index.js
+++ src/store/reducer/index.js
@@ -1,6 +1,5 @@
 import { combineReducers } from 'redux';
 import persistReducer from 'redux-persist/es/persistReducer';
-import autoMergeLevel2 from 'redux-persist/lib/stateReconciler/autoMergeLevel2';
 import storage from 'redux-persist/lib/storage';
 import authReducer from './authReducer';
 import homeReducer from './homeReducer';
@@ -8,22 +7,13 @@ import homeReducer from './homeReducer';
 const authPersistConfig = {
     key: 'auths',
     storage,
-    whitelist: ['token']
+    whitelist: ['token', 'user']
 };
-
-const rootPersistConfig = {
-    key: 'root',
-    storage,
-    stateReconciler: autoMergeLevel2,
-    whitelist: ['auths']
-}
 
 const rootReducer = combineReducers({
     auths: persistReducer(authPersistConfig, authReducer),
     home: homeReducer
 });
 
-const persistedReducer = persistReducer(rootPersistConfig, rootReducer);
-
-export default persistedReducer
+export default rootReducer
 
