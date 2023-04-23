6d65bd7c91f14b4c918c1690a6b12286c6206a6d
DvMinhGithub <dvminh.it@gmail.com>
2023-04-23 14:02:12 +0700

    Fix get data cart

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ import { persistor, store } from './store';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
-    <React.StrictMode>
+    // <React.StrictMode>
         <Provider store={store}>
             <PersistGate persistor={persistor}>
                 <Router>
@@ -25,7 +25,7 @@ root.render(
                 </Router>
             </PersistGate>
         </Provider>
-    </React.StrictMode>
+    // </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
